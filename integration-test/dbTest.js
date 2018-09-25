cccad1067ee476077a4e09e030174e4b29b20594
Marcos Silveira <bymarkone@gmail.com>
2018-09-25 14:55:22 -0300

    Use different database for testing

--- integration-test/dbTest.js
+++ integration-test/dbTest.js
@@ -1,15 +1,33 @@
-process
+process.env.DATADIR = './testdata'
 const chai = require('chai'),
       should = chai.should(),
       repo = require('../src/repository')
 
 describe('Repository tests', () =>  { 
-  describe('Add data', () => {
-      it('should add data to repository', async () => {
+  describe('Add and retrieve string data', () => {
+      it('should add string data to repository', async () => {
         await repo.put(1, 'Manfred')
-        const actual = await repo.get(1)
+        await repo.put(2, 'Maria')
+        const actual1 = await repo.get(1)
+        const actual2 = await repo.get(2)
 
-        actual.should.equal('Manfred')
+        actual1.should.equal('Manfred')
+        actual2.should.equal('Maria')
+      })
+
+      it('should add json data to repository', async () => {
+        let first = { name: 'John' }
+        let second = { name: 'Larry' }
+
+        await repo.putJson(1, first)
+        await repo.putJson(2, second)
+      
+        const actual1 = await repo.getJson(1)
+        const actual2 = await repo.getJson(2)
+
+        actual1.should.deep.equal(first)
+        actual2.should.deep.equal(second)
+          
       })
   })
 })

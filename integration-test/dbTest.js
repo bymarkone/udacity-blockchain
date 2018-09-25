ce13e0a9b9d9411c9028006a8bc29e8c38f73e1d
Marcos Silveira <bymarkone@gmail.com>
2018-09-25 16:20:02 -0300

    Deletes all

--- integration-test/dbTest.js
+++ integration-test/dbTest.js
@@ -1,9 +1,11 @@
 process.env.DATADIR = './testdata'
+
 const chai = require('chai'),
       should = chai.should(),
       repo = require('../src/repository')
 
 describe('Repository tests', () =>  { 
+
   describe('Add and retrieve string data', () => {
       it('should add string data to repository', async () => {
         await repo.put(1, 'Manfred')
@@ -28,6 +30,21 @@ describe('Repository tests', () =>  {
         actual1.should.deep.equal(first)
         actual2.should.deep.equal(second)
           
+      })
+  })
+
+  describe('Deletes data', () => {
+      it('Deletes all data', async () => {
+        await repo.put(1, 'Manfred')
+        await repo.put(2, 'Maria')
+
+        let count = await repo.count()
+        count.should.be.equal(2)
+        
+        await repo.deleteAll() 
+        let newcount = await repo.count()
+        console.log(newcount)
+        newcount.should.be.equal(0)
       })
   })
 })
--- src/repository.js
+++ src/repository.js
@@ -2,14 +2,37 @@ const level = require('level'),
       dir = process.env.DATADIR || './chaindata'
       db = level(dir)
 
+const doCount = () => {
+    return new Promise(resolve => {
+      let height = 0
+      db.createKeyStream().on('data', () => height++)
+        .on('close', () => resolve(height))
+    })
+}
+
+const doDeleteAll = async () => {
+        let batch = []
+        await new Promise(resolve => {
+          db.createKeyStream()
+            .on('data', key => batch.push({ type: 'del', key: key }))
+            .on('close', resolve)
+        })
+          
+        db.batch(batch)  
+      }
+
 const put = async (key, value) => await db.put(key, value)
 const get = async (key) => await db.get(key)
 const putJson = async (key, value) => await db.put(key, JSON.stringify(value))
 const getJson = async (key) => JSON.parse(await db.get(key))
+const count = doCount
+const deleteAll = doDeleteAll
 
 module.exports = {
   put,
   get,
   putJson,
-  getJson
+  getJson,
+  count,
+  deleteAll
 }
